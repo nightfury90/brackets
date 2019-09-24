3ef6460f87ab5949a4a34c5de4b3618b45b7a48c
Dmitriy An <pancake0890@mail.ru>
2019-09-24 23:52:57 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let open = ['(', '{', '[', '1', '3', '5'];
+  let close = [')', '}', ']', '2', '4', '6'];
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) {
+      stack.push(str[i]);
+    } else if (str[i] === '|') {
+      if (stack[stack.length - 1] === '|') {
+        stack.pop();
+      } else {
+        stack.push('|');
+      }
+    } else if (str[i] == '7') {
+      if (stack[stack.length - 1] == '7') {
+        stack.pop();
+      } else {
+        stack.push('7');
+      }
+    } else if (str[i] == '8') {
+      if (stack[stack.length - 1] == '8') {
+        stack.pop();
+      } else {
+        stack.push('8');
+      }
+    } else {
+      if (close.indexOf(str[i]) == open.indexOf(stack[stack.length - 1])) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return (stack.length == 0);
 }
+
